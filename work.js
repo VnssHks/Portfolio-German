70e461fa53cab7995b94e02ec533673f25fc7bb6
Vanessa Henkes <vahenkes@gmail.com>
2024-05-23 12:43:51 +0200

    Update work.js

--- work.js
+++ work.js
@@ -1,8 +1,7 @@
-// JavaScript to handle clicking on SVGs and redirecting
 $(document).ready(function() {
   $('.work .description').on('click', function() {
     var url = $(this).attr('data-url');
-    window.location.href = url; // Redirect to the specified URL
+    window.location.href = url;
   });
 });
 
@@ -10,12 +9,13 @@ function filterByCategory(category) {
   console.log('Filtering by category:', category);
   $('.work').each(function() {
     if (category === 'all' || $(this).data('category') === category) {
-      $(this).show(); // Show the work item if it belongs to the selected category or if all categories are selected
+      $(this).show();
     } else {
-      $(this).hide(); // Otherwise, hide the work item
+      $(this).hide(); 
     }
   });
 }
+
 window.addEventListener('scroll', function() {
   var backToTopBtn = document.getElementById('back-to-top-btn');
   if (window.pageYOffset > 100) { 
